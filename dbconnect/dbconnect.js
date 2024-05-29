81350e3cb3ed7625b4598fd6f2aae651b6dd765b
ashwani5111 <100413630+ashwani5111@users.noreply.github.com>
2024-05-29 14:48:56 +0530

    Update dbconnect.js

--- dbconnect/dbconnect.js
+++ dbconnect/dbconnect.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 
   const connection = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
+  host: '3.111.171.146',
+  user: 'ec2-root',
   // port:3306,
-  password: 'Admin@12345',
-  database: 'payroll_jk'
+  password: 'Gourav@12345',
+  database: 'anything'
 });
 module.exports = { connection };
